019f3a301d40ca235d9815cc55ff6b1f03e771ac
onhope <chch4934@gmail.com>
2024-05-16 12:12:22 +0900

    19. 컬러 토글

---
+++ 19. color toggle/app.js
@@ -0,0 +1,14 @@
+const toggler = document.querySelector("#switch");
+
+toggler.addEventListener("click", () => {
+  // if(toggler.checked === true) {
+  //   document.body.style.backgroundColor = "black";
+  // } else {
+  //   document.body.style.backgroundColor = "white";
+  // }
+
+  toggler.checked === true
+  ? document.body.style.backgroundColor = "black"
+  : document.body.style.backgroundColor = "white";
+
+})
