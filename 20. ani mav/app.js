bac6fb8e33dd80958866a6a2e7964074eb432364
onhope <chch4934@gmail.com>
2024-05-20 20:20:16 +0900

    #

--- 20. ani mav/app.js
+++ 20. ani mav/app.js
@@ -1,6 +1,6 @@
-const toggle = document.getElementById("toggle");
-const nav = document.getElementById("nav");
+const toggle = document.getElementById('toggle');
+const nav = document.getElementById('nav');
 
 toggle.addEventListener("click", () => {
-  nav.classList.toggle("active");
+  nav.classList.toggle('active');
 })
