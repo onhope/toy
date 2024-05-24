03509a11d8637691a967b791271985ab42dbaeac
onhope <chch4934@gmail.com>
2024-05-24 17:23:31 +0900

    #

---
+++ 25. progress bar/app.js
@@ -0,0 +1,45 @@
+const progress = document.querySelector(".progress");
+const circles = document.querySelectorAll(".circle");
+const prevBtn  = document.querySelector("#prev");
+const nextBtn = document.querySelector("#next");
+
+let activeIndex = 1;
+
+nextBtn.addEventListener("click", () => {
+  activeIndex++;
+  if(activeIndex > circles.length) {
+    activeIndex = circles.length;
+  }
+  updateUI();
+})
+
+prevBtn.addEventListener("click", () => {
+  activeIndex--;
+  if(activeIndex < 1) {
+    activeIndex = 1;
+  }
+  updateUI();
+})
+
+function updateUI() {
+  circles.forEach((circle, index) => {
+    if(index < activeIndex) {
+      circle.classList.add("active");
+    } else {
+      circle.classList.remove("active");
+    }
+  })
+
+  const actives = document.querySelectorAll(".active");
+  progress.style.width = ((actives.length - 1) / (circles.length - 1)) * 100 + "%";
+
+  if(activeIndex === 1) {
+    prevBtn.disabled = true;
+  } else if(activeIndex === circles.length) {
+    nextBtn.disabled = true;
+  } else {
+    prevBtn.disabled = false;
+    nextBtn.disabled = false;
+  }
+}
+
