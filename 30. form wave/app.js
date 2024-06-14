bd3ca57fb048843dfc31e66bd864560f8b4ea91e
onhope <chch4934@gmail.com>
2024-06-14 17:20:00 +0900

    30. Form Wave

--- 30. form wave/app.js
+++ 30. form wave/app.js
@@ -4,7 +4,6 @@ allLebels.forEach((label) => {
   label.innerHTML = label.innerHTML
     .split("") // 문자열의 문자들이 한개씩 쪼개져서 배열로 반환
     .map((letter, index) => 
-      // `<span style="transition-delay:${index * 50}ms">${letter}</span>`
       `<span style="transition-delay:${index * 50}ms">${letter}</span>`
     )
   .join("");
